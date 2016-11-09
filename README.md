cbde54f29d2d74230a7b4e1dae696c75e64cd640
Shoichiro Sakaigawa <Shoichiro.Sakaigawa@gmail.com>
2016-11-10 03:25:01 +0900

    debugging

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TLStester
+
+
+cmdline Option
+httpsURL
+
+JVM Debugging Option
+-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog
+-Dorg.apache.commons.logging.simplelog.showdatetime=true
+-Dorg.apache.commons.logging.simplelog.log.org.apache.http=DEBUG
+-Dorg.apache.commons.logging.simplelog.log.org.apache.http.wire=ERROR
--- tlsTest/src/tlsTester/TLStester.java
+++ tlsTest/src/tlsTester/TLStester.java
@@ -8,6 +8,8 @@ import java.security.NoSuchAlgorithmException;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.config.Registry;
@@ -18,6 +20,7 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.ssl.SSLContexts;
+import org.apache.http.util.EntityUtils;
 /**
  * @author s.sakaigawa
  *
@@ -27,7 +30,7 @@ public class TLStester {
 	private static Registry<ConnectionSocketFactory> getRegistry() throws KeyManagementException, NoSuchAlgorithmException {
 	    SSLContext sslContext = SSLContexts.custom().build();
 	    SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext,
-	            new String[]{"TLSv1.2"}, null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
+	            new String[]{"TLSv1.2","TLSv1"}, null, SSLConnectionSocketFactory.getDefaultHostnameVerifier());
 	    return RegistryBuilder.<ConnectionSocketFactory>create()
 	            .register("http", PlainConnectionSocketFactory.getSocketFactory())
 	            .register("https", sslConnectionSocketFactory)
@@ -36,6 +39,7 @@ public class TLStester {
 
 	public static void main(String[] args) {
 	    try {
+
 	        //Set the https use TLSv1.2
 	        PoolingHttpClientConnectionManager clientConnectionManager = new PoolingHttpClientConnectionManager(getRegistry());
 	        clientConnectionManager.setMaxTotal(100);
@@ -44,8 +48,19 @@ public class TLStester {
 	        //Then you can do : client.execute(HttpGet or HttpPost);
 
 	        HttpGet httpget = new HttpGet(args[0]);
+	        System.out.println(httpget.getRequestLine());
+	        HttpResponse response = client.execute(httpget);
+
+	        HttpEntity entity = response.getEntity();
+	        System.out.println(response);
+	        EntityUtils.consume(entity);
+
+
 	    } catch (KeyManagementException | NoSuchAlgorithmException e) {
 	        e.printStackTrace();
-	    }
+	    }catch (Exception e) {
+			// TODO: handle exception
+	    	e.printStackTrace();
+		}
 	}
 }
